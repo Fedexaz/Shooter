fa343b241645237981530906c05623c62d06156d
Fede Lahoz <54594663+Fedexaz@users.noreply.github.com>
2021-12-04 13:12:31 -0300

    Add files via upload

---
+++ MiniMapa/SeguirObjeto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguirObjeto : MonoBehaviour
+{
+
+    public Transform objetivo;
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(objetivo.transform.position.x, 20, objetivo.transform.position.z);
+    }
+}
